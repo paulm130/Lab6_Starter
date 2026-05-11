59d2ce13a159143276d35133e813e5216a9f742e
paulm130 <158609527+paulm130@users.noreply.github.com>
2026-05-11 14:56:48 -0700

    fixed Uncaught ReferenceError

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -45,7 +45,7 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-		let article = elementRoot;
+		let article = document.querySelector("article");
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
